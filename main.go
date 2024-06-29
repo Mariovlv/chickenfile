90b26e3b0361d8d535829eabab6e9a0c252647cb
mariovlv <quintanatorresmario@gmail.com>
2024-06-28 21:45:48 -0600

    hashing file, improve html

--- main.go
+++ main.go
@@ -4,6 +4,9 @@ import (
 	"bytes"
 	"chickenfile/client"
 	"context"
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/hex"
 	"fmt"
 	"log"
 	"net/http"
@@ -26,9 +29,14 @@ func init() {
 	client.InitializeS3Client()
 }
 
+func generateHMAC(input, secret string) string {
+	h := hmac.New(sha256.New, []byte(secret))
+	h.Write([]byte(input))
+	return hex.EncodeToString(h.Sum(nil))
+}
+
 func upload(c echo.Context) error {
-	name := c.FormValue("name")
-	email := c.FormValue("email")
+	keyword := c.FormValue("keyword")
 
 	file, err := c.FormFile("file")
 	if err != nil {
@@ -46,15 +54,6 @@ func upload(c echo.Context) error {
 	}
 	defer src.Close()
 
-	/*
-		cfg, err := config.LoadDefaultConfig(context.TODO())
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		client := s3.NewFromConfig(cfg)
-	*/
-
 	client := client.GetS3Client()
 	uploader := manager.NewUploader(client)
 
@@ -63,11 +62,16 @@ func upload(c echo.Context) error {
 		return echo.NewHTTPError(http.StatusInternalServerError, "AWS_BUCKET_NAME not set in environment")
 	}
 
-	key := file.Filename
+	secretWord := os.Getenv("SECRET_WORD")
+	if secretWord == "" {
+		return echo.NewHTTPError(http.StatusInternalServerError, "SECRET_WORD not set in environment")
+	}
+
+	keyForAWS := generateHMAC(keyword, secretWord)
 
 	result, err := uploader.Upload(context.TODO(), &s3.PutObjectInput{
 		Bucket: aws.String(bucketName),
-		Key:    aws.String(key),
+		Key:    aws.String(keyForAWS),
 		Body:   src,
 	})
 
@@ -75,20 +79,20 @@ func upload(c echo.Context) error {
 		return echo.NewHTTPError(http.StatusInternalServerError, "Failed to upload file to S3")
 	}
 
-	return c.HTML(http.StatusOK, fmt.Sprintf("<p>File %s uploaded successfully to %s with fields name=%s and email=%s.</p><p>Uploaded to: %s</p>", file.Filename, bucketName, name, email, result.Location))
+	return c.HTML(http.StatusOK, fmt.Sprintf("<p>File %s uploaded successfully to %s with secretword=%s.</p><p>Uploaded to: %s</p>", file.Filename, bucketName, keyword, result.Location))
 }
 
 func download(c echo.Context) error {
-	// Access the form value
 	bodyWord := c.FormValue("word")
+
+	key := generateHMAC(bodyWord, os.Getenv("SECRET_WORD"))
 
 	client := client.GetS3Client()
 
 	bucketName := os.Getenv("AWS_BUCKET_NAME")
-	// Example S3 operation: List objects in a bucket
 	output, err := client.GetObject(context.TODO(), &s3.GetObjectInput{
 		Bucket: aws.String(bucketName),
-		Key:    aws.String(bodyWord),
+		Key:    aws.String(key),
 	})
 
 	if err != nil {
@@ -96,16 +100,12 @@ func download(c echo.Context) error {
 	}
 	defer output.Body.Close()
 
-	// Read the content of the file
 	buf := new(bytes.Buffer)
 	_, err = buf.ReadFrom(output.Body)
 	if err != nil {
 		return echo.NewHTTPError(http.StatusInternalServerError, "Failed to read file content")
 	}
 
-	// You can add additional logic to decode the hash, request the resource from the S3 bucket, etc.
-
-	// Return the form value as a response for demonstration purposes
 	return c.Blob(http.StatusOK, *output.ContentType, buf.Bytes())
 }
 
@@ -120,5 +120,5 @@ func main() {
 	e.POST("/upload", upload)
 	e.POST("/download", download)
 
-	e.Logger.Fatal(e.Start(":1323"))
+	e.Logger.Fatal(e.Start(":8080"))
 }
